1290c59fff502f394e772f5579409906551730b8
akshaymalik1995 <akshaymalik191@gmail.com>
2022-07-13 10:42:08 +0530

    Adding Dynamic Paths and Routes

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,9 @@
+const PortfolioProjectPage = () => {
+    return (
+      <div className="PortfolioProjectPage">
+        <h1>This is a dynamic Page nested inside the portfolio</h1>
+      </div>
+    )
+  }
+  
+  export default PortfolioProjectPage
