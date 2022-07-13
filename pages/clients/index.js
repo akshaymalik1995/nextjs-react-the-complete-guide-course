bb710d7b8c5eb2d671be5dff9e5bbac36247a0c9
akshaymalik1995 <akshaymalik191@gmail.com>
2022-07-13 11:46:50 +0530

    Navigating to Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,18 @@
+import Link from "next/link"
+
 const ClientPage = () => {
+  const clients = [
+    {id : "akshay", name : "Akshay Malik"},
+    {id : "anubhav", name : "Anubhav Nain"},
+  ]
     return (
       <div className="ClientPage">
         <h1>This is a Client Page</h1>
+        <ul>
+          {clients.map((client => (
+            <li key={client.id}><Link href={`/clients/${client.id}`}>{client.name}</Link></li>
+          )))}
+        </ul>
       </div>
     )
   }
