a56d427c92f503fd2573bb4165b5e5fa78193f05
akshaymalik1995 <akshaymalik191@gmail.com>
2022-07-13 10:59:35 +0530

    Extracting Dynamic Path Segment Data

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router"
+
 const PortfolioProjectPage = () => {
+  const router = useRouter()
+  console.log("pathname", router.pathname) // pathname /portfolio/[projectid]
+  console.log("query object", router.query) // query object {projectid: 'akshay'}
+  console.log("projectid", router.query.projectid) // projectid akshay
     return (
       <div className="PortfolioProjectPage">
         <h1>This is a dynamic Page nested inside the portfolio</h1>
