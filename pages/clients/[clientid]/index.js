8116d00f710fcf4f34704bc8a8b0a922514547fc
akshaymalik1995 <akshaymalik191@gmail.com>
2022-07-13 12:41:20 +0530

    Navigating Programatically

--- pages/clients/[clientid]/index.js
+++ pages/clients/[clientid]/index.js
@@ -1,10 +1,16 @@
 import {useRouter} from "next/router"
 const ClientProjectsPage = () => {
   const router = useRouter()
-  console.log(router.query) // {clientid: 'max'}
+  const buttonHandler = () => {
+    router.push({
+      pathname : "/clients/[clientid]/[clientprojectid]",
+      query : {clientid : "max", clientprojectid : "react-todo"}
+    })
+  }
     return (
       <div className="ClientProjectsPage">
         <h1>Here is a list of projects of a given client</h1>
+        <button onClick={buttonHandler}>Open React Todo Project</button>
       </div>
     )
   }
