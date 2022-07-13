60ada6565c91ac3fa6124b41ef4c9c849ce2d2cd
akshaymalik1995 <akshaymalik191@gmail.com>
2022-07-13 12:13:01 +0530

    A Different Way of Setting Link Hrefs

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -2,20 +2,22 @@ import Link from "next/link"
 
 const ClientPage = () => {
   const clients = [
-    {id : "akshay", name : "Akshay Malik"},
-    {id : "anubhav", name : "Anubhav Nain"},
+    { id: "akshay", name: "Akshay Malik" },
+    { id: "anubhav", name: "Anubhav Nain" },
   ]
-    return (
-      <div className="ClientPage">
-        <h1>This is a Client Page</h1>
-        <ul>
-          {clients.map((client => (
-            <li key={client.id}><Link href={`/clients/${client.id}`}>{client.name}</Link></li>
-          )))}
-        </ul>
-      </div>
-    )
-  }
-  
-  export default ClientPage
-  
+  return (
+    <div className="ClientPage">
+      <h1>This is a Client Page</h1>
+      <ul>
+        {clients.map((client => (
+          <li key={client.id}><Link href={{
+            pathname: "/clients/[clientid]",
+            query: { clientid: client.id }
+          }}>{client.name}</Link></li>
+        )))}
+      </ul>
+    </div>
+  )
+}
+
+export default ClientPage
